68db3dc37d1297c2f43c1907d89678fc24baa591
cuizhiwei <cuizhiwei@shenhuo.co>
2017-01-23 11:37:25 +0800

    第一次建项目

---
+++ TextDisplay/TextDisplay/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TextDisplay
+//
+//  Created by vr on 17/1/23.
+//  Copyright © 2017年 com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
