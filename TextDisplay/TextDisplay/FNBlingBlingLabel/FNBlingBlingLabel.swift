0d3f737a56ec90625096da63a12d647820ec251b
cuizhiwei <cuizhiwei@shenhuo.co>
2017-01-23 11:59:17 +0800

    添加渐渐显示效果

---
+++ TextDisplay/TextDisplay/FNBlingBlingLabel/FNBlingBlingLabel.swift
@@ -0,0 +1,160 @@
+//
+//  FNBlingBlingLabel.swift
+//  FNBlingBlingLabel
+//
+//  Created by Fnoz on 16/5/30.
+//  Copyright © 2016年 Fnoz. All rights reserved.
+//
+
+import UIKit
+
+public class FNBlingBlingLabel: UILabel{
+    public var appearDuration = 1.5
+    public var disappearDuration = 1.5
+    public var attributedString:NSMutableAttributedString?
+    public var needAnimation = false
+    
+    var displaylink:CADisplayLink?
+    var isAppearing:Bool = false
+    var isDisappearing:Bool = false
+    var isDisappearing4ChangeText:Bool = false
+    var beginTime:CFTimeInterval?
+    var endTime:CFTimeInterval?
+    var durationArray:NSMutableArray?
+    var lastString:NSString?
+    var textColorAlpha:CGFloat = 1
+    
+    override public var text: String? {
+        get {
+            if needAnimation {
+                return self.attributedString?.string
+            }
+            else {
+                return super.text
+            }
+        }
+        
+        set {
+            if needAnimation {
+                self.convertToAttributedString(newValue!)
+            }
+            else {
+                super.text = newValue
+            }
+        }
+    }
+    
+    override public var textColor: UIColor! {
+        didSet
+        {
+            if CGColorGetAlpha(self.textColor.CGColor) > 0 {
+                print("fffffffff: %f", CGColorGetAlpha(self.textColor.CGColor))
+                textColorAlpha = CGColorGetAlpha(self.textColor.CGColor)
+            }
+        }
+    }
+    
+    public func convertToAttributedString(text: NSString) {
+        lastString = text
+        if self.attributedText?.length > 0 {
+            disappear()
+            isDisappearing4ChangeText = true
+        }
+        else {
+            appear()
+        }
+    }
+    
+    public func appear() {
+        attributedString = NSMutableAttributedString.init(string: lastString! as String)
+        
+        let paragraphStyle = NSMutableParagraphStyle.init()
+        paragraphStyle.lineSpacing = 4
+        attributedString?.addAttributes([NSParagraphStyleAttributeName:paragraphStyle], range: NSMakeRange(0, lastString!.length))
+        
+        isAppearing = true
+        beginTime = CACurrentMediaTime()
+        endTime = CACurrentMediaTime() + appearDuration
+        displaylink?.paused = false
+        
+        initDurationArray(appearDuration)
+    }
+    
+    public func disappear() {
+        isDisappearing = true
+        beginTime = CACurrentMediaTime()
+        endTime = CACurrentMediaTime() + disappearDuration
+        displaylink?.paused = false
+        initDurationArray(disappearDuration)
+    }
+    
+    func initDurationArray(duration: Double) {
+        durationArray = NSMutableArray.init(array: [])
+        for(var i = 0; i < self.attributedString?.length ; i += 1) {
+//            let progress: CGFloat = CGFloat(arc4random_uniform(100))/100.0
+            let progress: CGFloat = CGFloat(i) / 100.0
+            durationArray?.addObject(progress * CGFloat(duration) * 0.5)
+        }
+        print(durationArray)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initVariable()
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    func initVariable() {
+        displaylink = CADisplayLink.init(target: self, selector: #selector(updateAttributedString))
+        displaylink?.paused = true
+        displaylink?.addToRunLoop(NSRunLoop.currentRunLoop(), forMode: NSRunLoopCommonModes)
+    }
+    
+    func updateAttributedString() {
+        let pastDuration = CACurrentMediaTime() - beginTime!
+        if isAppearing {
+            if pastDuration > appearDuration {
+                displaylink?.paused = true
+                isAppearing = false
+            }
+            
+            for(var i = 0; i < self.attributedString?.length ; i += 1) {
+                var progress:CGFloat = CGFloat((pastDuration - (durationArray![i] as! Double)) / (appearDuration * 0.5))
+                if progress>1 {
+                    progress = 1
+                }
+                if progress<0 {
+                    progress = 0
+                }
+                let color = self.textColor.colorWithAlphaComponent(progress * textColorAlpha)
+                attributedString?.addAttributes([NSForegroundColorAttributeName: color], range: NSMakeRange(i, 1))
+            }
+        }
+        if isDisappearing {
+            if pastDuration>disappearDuration {
+                displaylink?.paused = true
+                isDisappearing = false
+                if isDisappearing4ChangeText {
+                    isDisappearing4ChangeText = false
+                    self.appear()
+                }
+                return
+            }
+            for(var i = 0; i < self.attributedString?.length ; i += 1) {
+                var progress:CGFloat = CGFloat((pastDuration - (durationArray![i] as! Double))/(disappearDuration * 0.5))
+                if progress > 1 {
+                    progress = 1
+                }
+                if progress<0 {
+                    progress = 0
+                }
+                let color = self.textColor.colorWithAlphaComponent((1 - progress) * textColorAlpha)
+                attributedString?.addAttributes([NSForegroundColorAttributeName: color], range: NSMakeRange(i, 1))
+            }
+        }
+        attributedText = attributedString
+    }
+}
--- TextDisplay/TextDisplay/ViewController.swift
+++ TextDisplay/TextDisplay/ViewController.swift
@@ -10,14 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var blingBlingLabel:FNBlingBlingLabel!
+    let textArray = ["      轻轻的我走了，\n      正如我轻轻的来；\n      我轻轻的招手，\n      作别西天的云彩。",
+                     "Very quietly I take my leave, \nAs quietly as I came here; \nQuietly I wave good-bye, \nTo the rosy clouds in the western sky.",
+                     "      夏天的飞鸟\n      飞到我的窗前唱歌，\n      又飞去了。\n      秋天的黄叶，\n      它们没有什么可唱，\n      只叹息一声，\n      飞落在那里。",
+                     "Stray birds of summer come to my window to sing and fly away.\nAnd yellow leaves of autumn, which have no songs, flutter and fall there with a sign. "];
+    var i = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = UIColor.init(red: 42/255.0, green: 49/255.0, blue: 67/255.0, alpha: 1)
+        
+        blingBlingLabel = FNBlingBlingLabel.init(frame: CGRectMake(0, 0, 300, 200))
+        blingBlingLabel.needAnimation = true
+        blingBlingLabel.center = self.view.center
+        blingBlingLabel.numberOfLines = 0
+        blingBlingLabel.font = UIFont.systemFontOfSize(23)
+        blingBlingLabel.textColor = UIColor.init(white: 1, alpha: 0.7)
+        blingBlingLabel.userInteractionEnabled = true
+        self.view.addSubview(blingBlingLabel)
+        blingBlingLabel.text = textArray[(i++)%textArray.count]
+        
+        let tap = UITapGestureRecognizer.init(target: self, action: #selector(ViewController.changeText))
+        blingBlingLabel?.addGestureRecognizer(tap)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func changeText() {
+        blingBlingLabel?.text = textArray[(i++)%textArray.count]
     }
 
 
