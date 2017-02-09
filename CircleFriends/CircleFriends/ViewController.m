a21a866fc3471d41bd4087fc00b55558615a2c5b
cuizhiwei <cuizhiwei@shenhuo.co>
2017-02-09 17:55:07 +0800

    第一次提交

---
+++ CircleFriends/CircleFriends/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CircleFriends
+//
+//  Created by vr on 17/2/9.
+//  Copyright © 2017年 com. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
